d8660596b12f9b5ea6f3b7b90dcb0db80c7c56ef
petr <petr@kulinar>
2017-12-28 18:34:04 +0300

    Fixed EOL

--- tm_to_gr_type1/index.js
+++ tm_to_gr_type1/index.js
@@ -183,11 +183,11 @@ try {
     Object.keys(grammar).forEach((key) => {
         if (Array.isArray(grammar[key]) === true) {
             for (let i = 0; i < grammar[key].length; ++i) {
-                writeStream.write(`${key} -> ${grammar[key][i]}\n`);
+                writeStream.write(`${key} -> ${grammar[key][i]}\r\n`);
             }
         }
         else {
-            writeStream.write(`${key} -> ${grammar[key]}\n`);
+            writeStream.write(`${key} -> ${grammar[key]}\r\n`);
         }
     });
     writeStream.end(() => {
